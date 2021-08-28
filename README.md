a983fe295e54876957482cd444ffc3a294f15b0a
Jesse Wilson <jessewilson@outlook.com>
2021-08-29 11:24:40 +1200

    Fixed line/outline bug  - need to check for any blur errors

--- README.md
+++ README.md
@@ -9,12 +9,14 @@ An interactive version of Conway's Game of Life
 - dynamically resize canvas and grid on window resize
     - if possible we don't want to destroy current cells on resize
 
+[done - but will need to look into possible blur problem]
 - debug draw and canvas so we don't see any lines/outlines / cells are cleaner
 - create container div/section to have canvas in
     - canvas should be centered
     - canvas should be width math.floor(window width / resolution)
         - e.g. cells should be resolution wide - not resolution + remainder
 
+[do this last - it might not be needed with the living for n gen change]
 - Trial mouse activacting 4 squares
     - this might be a togglable setting that users can play with later
 - Trial mouse cells living for N generations before being a) affected by GoL logic, or b) being able to die
--- script.js
+++ script.js
@@ -1,43 +1,31 @@
-let resolution = 10;
-let canvasWidth = window.innerWidth;
-let canvasHeight = window.innerHeight;
-// let canvasCells = canvasWidth * canvasHeight;
-let gridWidth = Math.floor(canvasWidth / resolution);
-let gridHeight = Math.floor(canvasHeight / resolution);
-// let gridCells = gridWidth * gridHeight;
-let cellWidth = canvasWidth / gridWidth;
-let cellHeight = canvasHeight / gridHeight;
-let refreshRate = 200;
-
+let resolution = 50; 
+let gridWidth = Math.floor(window.innerWidth / resolution);
+let gridHeight = Math.floor(window.innerHeight / resolution);
+let refreshRate = 500;
 
 let canvas = document.querySelector("#canvas");
-canvas.width  = canvasWidth;
-canvas.height = canvasHeight;
+canvas.width  = gridWidth * resolution;
+canvas.height = gridHeight * resolution;
 
 let ctx = canvas.getContext('2d');
 
-let grid = Array.from(Array(gridWidth), () => new Array(gridHeight).fill(0)); //this should be a function
+let grid = Array.from(Array(gridWidth), () => new Array(gridHeight).fill(0));
 
 document.onmousemove = mouseDraw;
 
 function draw () {
     for (let i = 0; i < gridWidth; i++) {
         for (let j = 0; j < gridHeight; j++) {
-            if (grid[i][j]) {
-                ctx.fillStyle = 'green';
-                ctx.fillRect(i * cellWidth, j * cellHeight, cellWidth, cellHeight);
-            } else {
-                ctx.fillStyle = 'white';
-                ctx.fillRect(i * cellWidth, j * cellHeight, cellWidth, cellHeight);
-            }
+            ctx.fillStyle = (grid[i][j])?  'green' : 'white'; 
+            ctx.fillRect(i * resolution, j * resolution, resolution, resolution);
         }
     }
 }
 
 
 function mouseDraw (e) {
-    let x = Math.floor(e.clientX / cellWidth);
-    let y = Math.floor(e.clientY / cellHeight);
+    let x = Math.floor(e.clientX / resolution);
+    let y = Math.floor(e.clientY / resolution);
     grid[x][y] = 1;
     draw()
 }
